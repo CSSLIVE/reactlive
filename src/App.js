581273b5ff62afe8e07a49ba2d73102cd3153ef0
smartphp <3238355967@qq.com>
2018-12-27 11:41:18 +0800

    **key**
    *** 简短说明干了什么**
     What:
    
    ---------------------------------------
    *** 为什么这么做?**
    Why:
    
    ***怎么解决的**
    How:
    
    ---------------------------------------
    
    ``
    
    ---------------------------------------
    
    ````

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import styled from 'styled-components';
+import {LiveCode}  from './Component/LiveCode';
+import {BigScreen} from './Component/Jumptron';
+//import {Counter}  from './Component/Counter';
+import {MainNav} from './Component/NavaBar';
+import {code1,code2,code3,code4,code5,code6,code7,subGrid,cssGrid} from './Component/Code';
+const scope= {styled};
+const MainContainer=styled.div`
+  width:85%;
+  margin-left:auto;
+  margin-right:auto;
+  height:100%;
+  overflow:hidden;
+  margin-top:1px;
+
+`
+
+class App extends Component {
+  
+  render() {
+    return (
+    
+      <MainContainer>
+        
+       <MainNav/>
+          <section>
+              <LiveCode code={code1} scope={scope} content="First Component"></LiveCode>
+              <LiveCode code={code2} scope={scope}></LiveCode>
+              <LiveCode code={code3} scope={scope}></LiveCode>
+              <LiveCode code={code4} scope={scope}></LiveCode>
+              <LiveCode code={code5} scope={scope}></LiveCode>
+              <LiveCode code={code6} scope={scope}></LiveCode>
+              <LiveCode code={code7} scope={scope}></LiveCode>
+              <LiveCode code={subGrid} scope={scope} content="subgrid"></LiveCode>
+              <LiveCode code={cssGrid} scope={scope} content="cssGrid"></LiveCode>
+              
+          </section>
+
+       <BigScreen></BigScreen> 
+       
+       
+      </MainContainer>
+       
+    
+    );
+  }
+}
+
+export default App;
